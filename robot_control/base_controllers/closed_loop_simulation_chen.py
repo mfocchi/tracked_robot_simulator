fixing energy computation
adding printfs to debug
commenting time

robot_control/base_controllers/closed_loop_simulation_chen.py
@@ -310,7 +310,7 @@ class GenericSimulator(BaseController):
             self.ros_pub.add_marker(np.array([blob_x, blob_y, self.basePoseW[2]]), color="white", radius=0.5)
 
     def getChomp(self, start, goal):
-        from tracked_robot.planners.chomp_no_theta import ChompSolver, Params
+        from tracked_robot.planners.chomp_no_theta import ChompSolverSlip, Params
         ch = ChompSolver()
         # -------------------------------
         # 1) Create a map
@@ -893,11 +893,11 @@ class GenericSimulator(BaseController):
 
         if self.TERRAIN: #this is published to show mesh in rviz
             if self.IDENT_TYPE=='WHEELS' and self.SIMULATOR=='distributed3d':
-                self.ros_pub.add_plane(pos=np.array([0,0,-0.]), orient=np.array([0., self.RAMP_INCLINATION, 0]), color="white", alpha=0.5)
+                self.ros_pub.add_plane(pos=np.array([0,0,-0.]), orient=np.array([0., self.RAMP_INCLINATION, 0]), color="white", alpha=1.0)
             else:
-                self.ros_pub.add_mesh("tractor_description", "/meshes/"+self.TERRAIN_TYPE+".stl", position=np.array([0., 0., 0.0]), color="red", alpha=1.0)
+                self.ros_pub.add_mesh("tractor_description", "/meshes/"+self.TERRAIN_TYPE+".stl", position=np.array([0., 0., 0.0]), color="red", alpha=0.5)
                 if self.OBSTACLES:
-                    self.ros_pub.add_mesh("tractor_description", '/meshes/obstacles.stl', position=np.array([0., 0., 0.0]), color="blue", alpha=1.0)
+                    self.ros_pub.add_mesh("tractor_description", '/meshes/obstacles.stl', position=np.array([0., 0., 0.0]), color="blue", alpha=0.6)
         if np.mod(self.time,1) == 0:
             print(colored(f"TIME: {self.time}","red"))
         self.pub_counter+=1

robot_control/base_controllers/evaluate_energy_consumption.py
@@ -266,8 +266,8 @@ class EvaluateEnergyConsumption(GenericSimulator):
             #   Fx * v_x     -> longitudinal traction effort
             #   Fx * beta    -> longitudinal slip loss
             #   Fy * v_y     -> lateral slip loss
-            P_long_left = Fx_l * (v_track_l + self.beta_l)
-            P_long_right = Fx_r * (v_track_r + self.beta_r)
+            P_long_left = Fx_l * ( self.beta_l)
+            P_long_right = Fx_r * (self.beta_r)
             P_lat_left = Fy_l * b_v_y
             P_lat_right = Fy_r * b_v_y
 
@@ -280,11 +280,11 @@ class EvaluateEnergyConsumption(GenericSimulator):
                 Energy_last = Energy
 
             if traj_finished and self.DEBUG:
-                print(f"Energy:{Energy} =  Cost  {sum(Cost)}, len of cost vector: {len(Cost)}")
+                print(f"Energy:{Energy} =  Cost")
                 break
 
-            if np.mod(self.time, 1) == 0:
-                print(colored(f"TIME: {self.time}", "red"))
+            # if np.mod(self.time, 1) == 0:
+            #     print(colored(f"TIME: {self.time}", "red"))
 
             if self.DEBUG:
                 self.logData()
@@ -294,8 +294,8 @@ class EvaluateEnergyConsumption(GenericSimulator):
 
 
 
-        if self.DEBUG:
-            self.plotData()
+        # if self.DEBUG:
+        #     self.plotData()
 
         return np.array(Cost)
 